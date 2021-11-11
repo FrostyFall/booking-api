9e1602986c7cbcf2be99bcb04a2fc04cae04e4f9
Artem Yakovlev <thenightfall808@icloud.com>
2021-11-11 18:39:12 +0300

    Add validation for 'rooms' routes

--- src/api/validation/index.js
+++ src/api/validation/index.js
@@ -1,5 +1,11 @@
 const authSchemas = require('./authSchemas');
 const bookedRoomsSchemas = require('./bookedRoomsSchemas');
 const hotelsSchemas = require('./hotelsSchemas');
+const roomsSchemas = require('./roomsSchemas');
 
-module.exports = { authSchemas, bookedRoomsSchemas, hotelsSchemas };
+module.exports = {
+  authSchemas,
+  bookedRoomsSchemas,
+  hotelsSchemas,
+  roomsSchemas,
+};
